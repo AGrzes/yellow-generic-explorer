a70d39aa0b6723c1558777f5b2dec352bb1099ab
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-02 21:29:26 +0100

    Serving data and web contnet

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const httpPort = process.env.HTTP_PORT || 3000
 const express = require('express')
 const app = express()
 
+app.use('/data',express.static('data.json'))
+app.use(express.static('static'))
+app.use(express.static('generated'))
+app.get('*',(req,res)=>{
+  res.sendFile(`${__dirname}/static/index.html`)
+})
+
 app.listen(httpPort, function () {
   console.log(`Generic explorer listening on port ${httpPort}!`)
 })
