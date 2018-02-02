f71e5ade1ca2ff9d23b21ca96da855bb664f9d9f
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-02 21:40:05 +0100

    Wabpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'generated')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader'
+      ]
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        'file-loader'
+      ]
+    }]
+  },
+  watchOptions: {
+    poll: true
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      })
+  ],
+  resolve: {
+    alias: {
+       'vue$': 'vue/dist/vue.esm.js' 
+    }
+  }
+};
