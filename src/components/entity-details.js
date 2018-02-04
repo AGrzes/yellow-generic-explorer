4a365d26007f7f33aadc7e34817b91d5622d1c76
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-04 16:00:15 +0100

    Displaying relations

--- src/components/entity-details.js
+++ src/components/entity-details.js
@@ -6,6 +6,10 @@ Vue.component('yellow-entity-details', {
       <label class="col-3">{{attribute.label}}</label>
       <div class="col-9">{{attribute.value}}</div>
     </div>
+    <div class="row" v-for="relation in relations">
+      <label class="col-3">{{relation.label}}</label>
+      <div class="col-9"><yellow-entity-link :entity="relation.entity"></yellow-entity-link></div>
+    </div>    
   </div>`,
   props: {
     entity: {
@@ -19,6 +23,13 @@ Vue.component('yellow-entity-details', {
         name,
         label: _.startCase(name),
         value: this.entity[name]
+      })).value()
+    },
+    relations() {
+      return _(Object.getOwnPropertyNames(this.entity)).filter((name) => _.startsWith(name, '@') && name !== '__ob__').map((name) => ({
+        name,
+        label: _.startCase(name),
+        entity: this.entity[name]
       })).value()
     }
   }
