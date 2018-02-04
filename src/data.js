2bf953bebb3d06797b0678e0e87326879dc26a72
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-04 15:59:56 +0100

    Normalizing model and resolving relations

--- src/data.js
+++ src/data.js
@@ -1,3 +1,28 @@
 const axios = require('axios')
 const _ = require('lodash')
-module.exports = axios.get('/data').then(_.property('data'))
+module.exports = axios.get('/data').then(_.property('data')).then((data)=>{
+  const extractEntities = (entity)=>{
+    if (entity && _.isObject(entity)){
+      return [entity,..._(entity).flatMap((target,name)=>{
+        if (_.startsWith(name,'@')){
+          return extractEntities(target)
+        } else {
+          return []
+        }
+      })]
+    } else {
+      return []
+    }
+  }
+  const itemMap =_(data.items).flatMapDeep(extractEntities).keyBy(_.property('key')).value()
+  _.forEach(itemMap,(item)=>{
+    _.forEach(item,(target,name)=>{
+      if (_.startsWith(name,'@') &&_.isString(target) && itemMap[target]){
+        item[name] = itemMap[target]
+      }
+    })
+  })
+  return {
+    items: _.values(itemMap)
+  }
+})
