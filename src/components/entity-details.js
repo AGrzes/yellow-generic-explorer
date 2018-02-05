d2f7bd852dc4c122374acc0fd9e1cb47209c4326
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-05 21:22:20 +0100

    Styling entity defaults

--- src/components/entity-details.js
+++ src/components/entity-details.js
@@ -2,15 +2,17 @@ import Vue from 'vue'
 import _ from 'lodash'
 import {attributes,relations} from '../metadata'
 Vue.component('yellow-entity-details', {
-  template: `<div>
-    <div class="row" v-for="attribute in attributes">
-      <label class="col-3">{{attribute.label}}</label>
-      <div class="col-9">{{attribute.value}}</div>
-    </div>
-    <div class="row" v-for="relation in relations">
-      <label class="col-3">{{relation.label}}</label>
-      <div class="col-9"><yellow-entity-link :entity="relation.entity"></yellow-entity-link></div>
-    </div>    
+  template: `<div class="card">
+    <div class="card-body">
+      <div class="row" v-for="attribute in attributes">
+        <label class="col-2 text-right">{{attribute.label}}</label>
+        <div class="col-10">{{attribute.value}}</div>
+      </div>
+      <div class="row" v-for="relation in relations">
+        <label class="col-2 text-right">{{relation.label}}</label>
+        <div class="col-10"><yellow-entity-link :entity="relation.entity"></yellow-entity-link></div>
+      </div>  
+    </div>  
   </div>`,
   props: {
     entity: {
